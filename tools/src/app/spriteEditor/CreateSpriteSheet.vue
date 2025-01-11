27c8e35115828dc928c3444fac203653bb7b3ea3
Royal <royal@royaltheartist.com>
2025-01-10 17:05:06 -0700

    Load and view sprites, can send up sprite coords for sheet

--- tools/src/app/spriteEditor/CreateSpriteSheet.vue
+++ tools/src/app/spriteEditor/CreateSpriteSheet.vue
@@ -1,27 +1,51 @@
 <script setup lang="ts">
 import FileInput from '@/components/fileInput.vue';
 import TextInput from '@/components/TextInput.vue';
+import SpritesheetViewer from '@/modules/spritesheets/SpritesheetViewer.vue';
+import type { Rect } from 'bt-engine/utils';
+import { ref } from 'vue';
 
-const loadImage = async (event: any) => {
+const editImage = ref<HTMLImageElement | null>(null)
+const atlas = ref(new Set<Rect>())
+
+
+const loadImage = async (event: Event) => {
     const target = event.target as HTMLInputElement;
     if (target.files) {
         const image = new Image()
         image.src = URL.createObjectURL(target.files[0])
-        const preview = document.getElementById('preview')
-        preview?.appendChild(image)
+        editImage.value = image 
     }
 }
 
+const addSprite = (rect: Rect) => {
+    atlas.value.add(rect)
+}
+
+const deleteSprite = (rect: Rect) => {
+    for(const sprite of atlas.value) {
+        if (rect.x === sprite.x && rect.y === sprite.y) {
+            atlas.value.delete(sprite)
+            return
+        }
+    }
+}
 </script>
 
 <template>
     <section>
         <h3>New</h3>
-        <TextInput label="Name" />
         <FileInput placeholder="Load Image" @change="loadImage" accept=".png"></FileInput>
-        <div id="preview">
-            
+        <TextInput label="Name" />
+    
+        <div class="container" style="max-width: 500px">
+            <SpritesheetViewer 
+                :atlas="atlas" 
+                :image="editImage" 
+                @sprite:add="addSprite"
+                @sprite:delete="deleteSprite"></SpritesheetViewer>
         </div>
+        
     </section>
 </template>
 
---
+++ tools/src/modules/spritesheets/SpritesheetViewer.vue
@@ -0,0 +1,134 @@
+<script setup lang="ts">
+import { Surface } from 'bt-engine/render';
+import {  Rect, Vector2D } from 'bt-engine/utils';
+import { onMounted, onUnmounted, useTemplateRef, watch } from 'vue';
+import { MouseHandler } from "bt-engine/input/mouse"
+import { Colors } from 'bt-engine/graphics/colors'
+
+const target = useTemplateRef('target')
+
+const props = defineProps<{
+    image: HTMLImageElement | null,
+    atlas: Set<Rect>
+}>()
+
+const emits = defineEmits<{
+    (e: 'sprite:add', sprite: Rect): Rect
+    (e: 'sprite:delete', sprite: Rect): Rect
+}>()
+
+class SpritesheetViewer {
+    surface: Surface
+    mouse: MouseHandler
+    gridHeight: number = 16
+    gridWidth: number = 16
+    atlas: Set<Rect> = new Set()
+    _img: HTMLImageElement | null = null
+    _selectFn: (rect: Rect) => void | null = null
+
+    constructor(width: number = 500, height: number = 500) {
+        this.surface = new Surface(new Vector2D(width, height)).initialize()
+        this.surface.context.imageSmoothingEnabled = false
+
+        this.mouse = new MouseHandler()
+        this.mouse.init(this.surface.canvas)
+
+        this.mouse.onMouseDown(() => {
+            if (this._selectFn) {
+                this._selectFn(new Rect(this.lockedMousePos.x, this.lockedMousePos.y, this.gridWidth, this.gridHeight))
+            }
+        })
+    }
+
+    attachSurface(el: HTMLElement) {
+        el.appendChild(this.surface.canvas)
+    }
+
+    draw() {
+        this.surface.clear()
+        this.surface.drawRect(new Vector2D(0, 0), new Vector2D(this.surface.width, this.surface.height), Colors.black)
+        if (this._img) {
+            this.surface.drawZoom(this._img, new Vector2D(0, 0), 2)
+            this.drawSelected()
+        }
+
+        this.drawMouse(this.mouse)
+    }
+
+    drawSelected() {
+        for (const rect of this.atlas) {
+            this.surface.drawStrokeRect(new Vector2D(rect.x * this.zoomedGrid.x, rect.y * this.zoomedGrid.y), this.zoomedGrid, Colors.yellow)
+        }
+    }
+
+    drawMouse(mouse: MouseHandler) {
+        if (!mouse.available) return
+
+        const lockedCoords = this.lockedMousePos
+
+        this.surface.context.strokeStyle = 'red'
+        this.surface.context.strokeRect(lockedCoords.x * this.zoomedGrid.x, lockedCoords.y * this.zoomedGrid.y, this.zoomedGrid.x, this.zoomedGrid.y)
+
+        this.surface.drawText(`(${lockedCoords.x}, ${lockedCoords.y})`, new Vector2D(5, 5), Colors.white)
+    }
+
+    onSelectSprite(fn: (rect: Rect) => void) {
+        this._selectFn = fn
+    }
+
+    public get lockedMousePos() {
+        const lockedCoords = {
+            x: Math.floor(( this.mouse.mousePos.x + 3) / this.zoomedGrid.x),
+            y: Math.floor(( this.mouse.mousePos.y + 3) / this.zoomedGrid.y)
+        } 
+        return lockedCoords
+    }
+
+    public get zoomedGrid() {
+        return new Vector2D(this.gridWidth * 2, this.gridHeight * 2)
+    }
+}
+
+const spriteSheetViewer = new SpritesheetViewer()
+
+spriteSheetViewer.onSelectSprite((rect: Rect) => {
+    for (const sprite of props.atlas) {
+        if (rect.x === sprite.x && rect.y === sprite.y) {
+            emits('sprite:delete', rect)
+            return
+        }
+    }
+    emits('sprite:add', rect)
+})
+
+watch(
+    () => props.image,
+    () => {
+        spriteSheetViewer._img = props.image
+        spriteSheetViewer.draw()
+    }
+)
+
+let requestId: number
+
+const render = () => {
+    spriteSheetViewer.atlas = props.atlas
+    spriteSheetViewer.draw()
+    requestId = requestAnimationFrame(render)
+}
+
+onMounted(() => {
+    if(!target.value) return
+    spriteSheetViewer.attachSurface(target.value)
+
+    render()
+})
+
+onUnmounted(() => {
+    if (requestId) cancelAnimationFrame(requestId)
+})
+</script>
+
+<template>
+    <div ref="target"></div>
+</template>
